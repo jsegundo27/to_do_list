a663639491de05919fb1baeaa59c743521806e1a
jsegundo27 <edwin27segundo@gmail.com>
2025-04-23 10:10:37 -0500

    metodos desarrollados y probados create,update,eliminar

---
+++ php/tarea/tareas_eliminar.php
@@ -0,0 +1,24 @@
+<?php
+
+require ("../../config/conexion.php");
+$codigo=$_POST["codigo"];
+
+$cnn=conectarDataBase();
+
+
+
+$sql="DELETE FROM tarea where id = ?";
+
+$smt = $cnn->prepare($sql);
+
+$smt->bind_param("i",$codigo);
+
+$smt->execute();
+
+if($smt->affected_rows>0){
+
+echo "se elimino correctamente";
+
+}else{
+echo "Problemas con el registro";
+}
