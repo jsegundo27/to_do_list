cf7c6b856c49f9a845afabf6a70a0ab5555248b3
jsegundo27 <edwin27segundo@gmail.com>
2025-04-24 08:55:19 -0500

    Cambios hecho por Jonathan Dev

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<!--Este codigo esta modificado por jonatha-dev -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To do List</title>
