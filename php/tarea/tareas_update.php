7fc0e87bc91283b2a4a42206fc55a857e34f84c0
jsegundo27 <edwin27segundo@gmailcom>
2025-04-25 07:07:22 -0500

    modifique el tareas_updae para no perder la construnbre

--- php/tarea/tareas_update.php
+++ php/tarea/tareas_update.php
@@ -2,26 +2,34 @@
 
 require ("../../config/conexion.php");
 
-$codigo =$_POST["codigo"];
-$titulo =$_POST["titulo"];
-$descripcion =$_POST["descripcion"];
-
-$cnn=conectarDataBase();
-
-$sql="UPDATE tarea set titulo= ? , descripcion = ? WHERE id = ? ";
-
-$smt=$cnn->prepare($sql);
-if (!$smt) {
-    die("error:".$cnn->error);
+if($_SERVER('REQUEST_METHOD') !=='POST'){
+    echo 'Metodo no permitido';
 }
-$smt->bind_param("ssi",$titulo,$descripcion,$codigo);
 
-$smt->execute();
-if($smt->affected_rows>0){
+if (!empty($_POST["codigo"]) && !empty($_POST['titulo']) && !empty($_POST['descripicion'])) {
+   $codigo =$_POST["codigo"];
+   $titulo =$_POST["titulo"];
+   $descripcion =$_POST["descripcion"];
 
-   echo json_encode("se registro correctamente");
+   $cnn=conectarDataBase();
 
-}else{
-   echo json_encode("Problemas con el registro");
+   $sql="UPDATE tarea set titulo= ? , descripcion = ? WHERE id = ? ";
+
+   $smt=$cnn->prepare($sql);
+   if (!$smt) {
+      die("error:".$cnn->error);
+   }
+   $smt->bind_param("ssi",$titulo,$descripcion,$codigo);
+
+   $smt->execute();
+
+   if($smt->affected_rows>0){
+
+      echo json_encode("se registro correctamente");
+
+   }else{
+      echo json_encode("Problemas con el registro");
+   }
+
 }
 
